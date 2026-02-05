91190315a7d358e3c087350df0e20aa62ba5149f
developerwkddbs <happyoon913@gmail.com>
2026-02-05 17:25:41 +0900

    Feat: 가계부 기본 CRUD (입력/조회) 구현 RE

--- app.py
+++ app.py
@@ -1,40 +1,41 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
-app=Flask(__name__)
-DB_Name="account.db"
+app = Flask(__name__)
+DB_NAME = "account_v2.db"
 
 def get_db():
-    return sqlite3.connect(DB_Name)
+    return sqlite3.connect(DB_NAME)
 
-@app.route("/",methods=["GET","POST"])
+@app.route("/", methods=["GET", "POST"])
 def index():
-    conn=get_db()
-    cur=conn.cursor()
+    conn = get_db()
+    cur = conn.cursor()
 
-    #If no TABLE, CREATE TABLE
+    # 테이블 없으면 생성
     cur.execute("""
-            CREATE TABLE IF NOT EXISTS records (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                type TEXT NO NULL,      -- income / expense
-                ammount INTEGER NOT NULL,
-                memo TEXT,
-                created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                )
-            """)
-    
+        CREATE TABLE IF NOT EXISTS records (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            type TEXT NOT NULL,        -- income / expense
+            amount INTEGER NOT NULL,
+            memo TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """)
+
     if request.method == "POST":
-        r_type=request.form["type"]
-        amount=request.form["amount"]
-        memo=request.form["memo"]
+        r_type = request.form["type"]
+        amount = request.form["amount"]
+        memo = request.form["memo"]
 
         cur.execute(
-            "INSERT INTO records (type, amount, memo) VALUES (?,?,?)",
-            (r_type,amount,memo)
+            "INSERT INTO records (type, amount, memo) VALUES (?, ?, ?)",
+            (r_type, amount, memo)
         )
         conn.commit()
         conn.close()
         return redirect(url_for("index"))
+
     cur.execute("SELECT id, type, amount, memo, created_at FROM records ORDER BY id DESC")
     records = cur.fetchall()
     conn.close()
